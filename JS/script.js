99743b84b2a74d0dddc5c3ad2bbf0d63efdf998c
VONETTA <thevonetteway@gmail.com>
2022-03-13 17:56:10 +0300

    add functions to design

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,10 @@
-$(document).ready(){
-    $("#design-image").click(function); {
+$(document).ready();{
+    $("#design-image").click(function);{
         $("design-image").slideDown('1500').hide('1000');
         $("#design").show('1500');
             
     });
-    $("#design").click(function){
+    $("#design").click(function);{
         $("design").slideUp('1500');
         $("design-image").slideDown('1500');
     });
