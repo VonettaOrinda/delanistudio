8aae45304a624c2114b6e59396db08db6f9665a7
VONETTA <thevonetteway@gmail.com>
2022-03-13 17:16:46 +0300

    add to script.js the slide down and slide up function

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+$(document).ready(){
+    $("#design-image").click(function); {
+        $("design-image").slideDown('1500').hide('1000');
+        $("#design").show('1500');
+            
+    });
+    $("#design").click(function){
+        $("design").slideUp('1500');
+        $("design-image").slideDown('1500');
+    });
+}
