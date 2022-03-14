77d107abf7b2316f1c9c8fea6863d927192a81a8
VONETTA <thevonetteway@gmail.com>
2022-03-14 12:45:57 +0300

    add changes to js alert pop up

--- JS/script.js
+++ JS/script.js
@@ -1,67 +1,65 @@
-$(document).ready();{
-    $("#design-image").click(function(){
-        $("design-image").slideDown('1500').hide('1000');
-        $("#design").show('1500');
-            
-    });
-    $("#design").click(function(){
-        $("design").slideUp('1500');
-        $("design-image").slideDown('1500');
-    });
+$(document).ready(function(){
+  $("#development-image").click(function(){
+    $("#development-image").slideDown('1500').hide('1000');
+    $("#development").show('1500');
+  });
+  $("#development").click(function(){
+    $("#development").slideUp('1500');
+    $("#development-image").slideDown('1500');
+  });
+});
 
+$(document).ready(function(){
+  $("#design-image").click(function(){
+    $("#design-image").slideDown('1500').hide('1000');
+    $("#design").show('1500');
+  });
+  $("#design").click(function(){
+    $("#design").slideUp('1500');
+    $("#design-image").slideDown('1500');
+  });
+});
 
-$(document).ready();{
-    $("#development-image").click(function(){
-        $("#development-image").slideDown('1500').hide('1000');
-        $("#development").show('1500');
-
-      });
-      $("#development").click(function(){
-        $("#development").slideUp('1500');
-        $("#development-image").slideDown('1500');
-      });
-    
-
-$(document).ready();{$("#product-image").click(function(){
+$(document).ready(function(){
+  $("#product-image").click(function(){
     $("#product-image").slideDown('1500').hide('1000');
     $("#product").show('1500');
-
   });
   $("#product").click(function(){
     $("#product").slideUp('1500');
     $("#product-image").slideDown('1500');
   });
-
+});
 
 $(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#overlay").show();
-    }).mouseout(function(){
-      $("#overlay").hide();
-    });
+  $("#work1").mouseover(function(){
+    $("#overlay").show();
+  }).mouseout(function(){
+    $("#overlay").hide();
   });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
-    });
+});
+$(document).ready(function(){
+  $("#work2").mouseover(function(){
+    $("#overlay2").show();
+  }).mouseout(function(){
+    $("#overlay2").hide();
   });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
-    });
+});
+$(document).ready(function(){
+  $("#work3").mouseover(function(){
+    $("#overlay3").show();
+  }).mouseout(function(){
+    $("#overlay3").hide();
   });
-  $(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#overlay4").show();
-    }).mouseout(function(){
-      $("#overlay4").hide();
-    });
+});
+$(document).ready(function(){
+  $("#work4").mouseover(function(){
+    $("#overlay4").show();
+  }).mouseout(function(){
+    $("#overlay4").hide();
   });
-  
+});
+
 $(document).ready(function(){
   $("#work5").mouseover(function(){
     $("#overlay5").show();
@@ -83,25 +81,22 @@ $(document).ready(function(){
   }).mouseout(function(){
     $("#overlay8").hide();
   });
+});
 
-  $(document).ready(function(){
-    $("form#form34A").submit(function(event){
-      // event.preventDefault();
-      var name = $("input#MERGE1").val();
-      var email = $("input#MERGE0").val();
-      var message = $("textarea#comment").val();
-      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
-        alert (name + ", we have received your message. Thank you for reaching out to us.");
-      }
-      else {
-        alert("Please enter your name and email!");
-      }
-      
-    });
-  
+$(document).ready(function(){
+  $("form#form34A").submit(function(event){
+    // event.preventDefault();
+    var name = $("input#MERGE1").val();
+    var email = $("input#MERGE0").val();
+    var message = $("textarea#comment").val();
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+      alert (name + ", we have received your message. Thank you for reaching out to us.");
+    }
+    else {
+      alert("Please enter your name and email!");
+    }
+    
   });
 
-}
-}
-}
-}
+});
+
